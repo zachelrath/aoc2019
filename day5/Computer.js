064884e0e8042ca83f0bc0da944b6adc26e6d116
Zach McElrath <zach@skuid.com>
2019-12-09 19:51:04 -0500

    start day 5

---
+++ day5/Computer.js
@@ -0,0 +1,105 @@
+
+const MEMORY = Symbol("memory");
+
+const TYPE_ADD = 1,
+    TYPE_MULTIPLY = 2,
+    TYPE_SET = 3,
+    TYPE_GET = 4,
+    TYPE_DIE = 99;
+
+class Computer {
+    constructor(programCsv, noun, verb) {
+        this[MEMORY] = Computer.parse(programCsv);
+        if (typeof noun !== "undefined" && typeof verb !== "undefined") {
+            this[MEMORY][1] = noun;
+            this[MEMORY][2] = verb;
+        }
+    }
+
+    static parse(programCsv) {
+        return programCsv.split(",").map(x => parseInt(x, 10));
+    }
+
+    /**
+     * Sets value of a memory address
+     * @param {Integer} address 
+     * @param {Integer} value
+     * @returns {Computer}
+     */
+    set(address, value) {
+        this[MEMORY][address] = value;
+        return this;
+    }
+
+    /**
+     * Returns the value at a given address
+     * @param {Integer} address
+     * @returns {Integer}
+     */
+    get(address) {
+        return this[MEMORY][address];
+    }
+
+    /**
+     * Runs the instruction set against the current memory
+     * @returns {Computer}
+     */
+    run() {
+        const state = this[MEMORY],
+            length = state.length;
+
+        let address = 0,
+            paramA,
+            paramB,
+            targetAddress;
+        while (address < length) {
+
+            let opCode = state[address];
+
+            if (opCode === TYPE_DIE) break;
+
+            if ([TYPE_ADD, TYPE_MULTIPLY].includes(opCode)) {
+                let paramA = state[state[address + 1]],
+                    paramB = state[state[address + 2]],
+                    targetAddress = state[address + 3],
+                    newValue;
+
+                if (opCode === TYPE_ADD) {
+                    // add parameters
+                    newValue = paramA + paramB;
+                } else if (opCode === TYPE_MULTIPLY) {
+                    // multiply parameters
+                    newValue = paramA * paramB;
+                }
+                // Store result at target address
+                state[targetAddress] = newValue;
+                // Next code should be 4 positions away
+                address += 4;
+            } else if ([TYPE_GET, TYPE_SET].includes(opCode)) {
+                let paramA = state[address[1]];
+                // If using positional mode...
+                let valAtPosition = state[paramA];
+                if (opCode === TYPE_GET) {
+                    // output requested parameter
+                    console.log(paramA);
+                } else if (opCode === TYPE_SET) {
+                    // in positional mode, set value at position TO the param
+                    state[paramA] = paramA;
+                }
+                // Next code should be 2 positions away
+                address += 2;
+            }
+        }
+        return this;
+    }
+    /**
+     * Return the program state as a CSV string
+     * @returns {String}
+     */
+    serialize() {
+        return this[MEMORY].join(",");
+    }
+
+}
+
+module.exports = Computer;
